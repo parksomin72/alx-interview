af2b9f26dbb16e8461bd8c67d9a9cac732026bf6
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-16 16:57:50 +0000

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -18,13 +18,3 @@ def canUnlockAll(boxes):
 
     # Check if all boxes have been visited
     return len(visited) == len(boxes)
-
-# Test cases
-boxes1 = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes1))  # Output: True
-
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes2))  # Output: True
-
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes3))
